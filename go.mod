2f6effcffb4f5d3144d180a827385b21833597a6
johnsonsmile <johnsonsmile@163.com>
2025-08-21 14:17:49 +0800

    feat: wallet and address

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module github.com/matrix-go/bitcoin
 
 go 1.24.1
+
+require (
+	github.com/btcsuite/btcutil v1.0.2
+	golang.org/x/crypto v0.41.0
+)
--- main.go
+++ main.go
@@ -2,24 +2,29 @@ package main
 
 import (
 	"fmt"
-	"github.com/matrix-go/bitcoin/core"
+	"github.com/matrix-go/bitcoin/wallet"
 )
 
 func main() {
 
-	blockchainAddress := "miner"
-	bc := core.NewBlockchain(blockchainAddress)
-	bc.Print()
+	//blockchainAddress := "miner"
+	//bc := core.NewBlockchain(blockchainAddress)
+	//bc.Print()
+	//
+	//bc.AddTransaction("A", "B", 10)
+	//bc.Mining()
+	//bc.Print()
+	//
+	//bc.AddTransaction("C", "D", 20)
+	//bc.AddTransaction("X", "Y", 30)
+	//bc.Mining()
+	//bc.Print()
+	//
+	//fmt.Printf("C: %d\n", bc.CalculateTotalAmount("C"))
+	//fmt.Printf("D: %d\n", bc.CalculateTotalAmount("D"))
 
-	bc.AddTransaction("A", "B", 10)
-	bc.Mining()
-	bc.Print()
-
-	bc.AddTransaction("C", "D", 20)
-	bc.AddTransaction("X", "Y", 30)
-	bc.Mining()
-	bc.Print()
-
-	fmt.Printf("C: %d\n", bc.CalculateTotalAmount("C"))
-	fmt.Printf("D: %d\n", bc.CalculateTotalAmount("D"))
+	w := wallet.NewWallet()
+	fmt.Println(w.PrivateKeyStr())
+	fmt.Println(w.PublicKeyStr())
+	fmt.Println(w.Address())
 }
